766275fda004b0dccb71a7f172865f198bdf1360
Brandon <brandonkly@ucla.edu>
2022-12-03 14:20:08 -0800

    added TODO's

---
+++ index.js
@@ -0,0 +1,12 @@
+// TODO: npm install jest
+// TODO: create unit tests for application
+
+// TODO: npm install inquirer@8.2.4
+// TODO: question for team members and info
+// TODO: generate html
+// TODO: mailto email anchor tags, github profile anchor tags
+// TODO: on start, enter manager name, employee id, email, and office number
+// TODO: on manager info, can add engineer or intern to team, or finish team
+// TODO: on engineer, question for name, id, email, github username, then return
+// TODO: on intern, name, id, email, school, then return
+// TODO: on finish team, exit terminal application and html is generated

903d7cc6491185a27a0fd8583d90d1516d126a09
Brandon <brandonkly@ucla.edu>
2022-12-03 17:41:38 -0800

    added starter code

--- index.js
+++ index.js
@@ -11,3 +11,11 @@
 // TODO: on intern, name, id, email, school, then return
 // TODO: on finish team, exit terminal application and html is generated
 // TODO: add validation to ensure user inputs are correctly formatted
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs

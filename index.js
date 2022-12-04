52cf3d22313a7741fbd50a4b117e465ec19ea9c4
Brandon <brandonkly@ucla.edu>
2022-12-03 18:58:47 -0800

    create html file and full question list

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Employee = require('./lib/Employee');
+const employees = [];
 
 //TODO - write your inquirer app here to gather information about the team members, and generate the HTML file using fs
-newEmployee()
 function newEmployee() {
-    inquirer.createPromptModule([
+    return inquirer.prompt([
         {
             type:'list',
             name: 'position',
-            message: 'What position is this employee?',
+            message: "What is the employee's positon?",
             choices: [
                 'Manager',
                 'Intern',
@@ -36,52 +36,93 @@ function newEmployee() {
         {
             type:'input',
             name: 'name',
-            message: 'What name is this employee?',
+            message: "What is the employee's name?",
         },
         {
             type:'input',
             name: 'id',
-            message: 'What id is this employee?',
+            message: 'What is their id?',
         },
         {
             type:'input',
             name: 'email',
-            message: 'What email is this employee?',
+            message: 'What is their email?',
         },
     ]).then(({position, name, id, email}) => {
         switch(position) {
             case 'Manager':
-                inquirer.createPromptModule([
+                inquirer.prompt([
                     {
                         type: 'input',
                         name: 'officeNumber',
-                        message: 'What is the office number?',
+                        message: 'What is their office number?',
                     }
                 ]).then(({officeNumber}) => {
                     employees.push(new Manager(name, id, email, officeNumber))
-                })
-    
+                    another()
+                });
                 break;
             case 'Intern':
-    
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'school',
+                        message: 'What is their school name?',
+                    }
+                ]).then(({school}) => {
+                    employees.push(new Intern(name, id, email, school))
+                    another()
+                });
                 break;
             case 'Engineer':
-    
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'github',
+                        message: 'What is their github?',
+                    }
+                ]).then(({github}) => {
+                    employees.push(new Engineer(name, id, email, github))
+                    another()
+                });
                 break;
             default:
         }
-        inquirer.createPromptModule() [
-            {
-                type: 'confirm',
-                name: 'continue',
-                message: 'Would you like to create another employee card?'
-            }
-        ].then(({more}) => {
-            if (more) {
-                newEmployee();
-            } else {
-                renderHTML();
-            }
-        })
     })
 }
+
+function another() {
+    return inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'more',
+            message: 'Would you like to create another employee card?'
+        }
+    ]).then(({more}) => {
+        if (more) {
+            newEmployee();
+        } else {
+            renderHTML();
+            return console.log(employees)
+        }
+    })
+}
+
+function renderHTML() {
+    fs.writeFileSync('./index.html',
+    `<ul>
+    ${employees.map(employee => 
+        `<li>
+            <div>
+                <h2>${employee.getName()}</h2>
+                <p>${employee.getRole()}</p>
+                <p>${employee.getId()}</p>
+                <p>${employee.getEmail()}</p>
+            </div>
+        </li>`
+        )}
+    </ul>`
+    )
+}
+
+newEmployee()
